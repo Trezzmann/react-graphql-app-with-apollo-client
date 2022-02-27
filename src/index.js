8770c56bb7b4227d0f6694924b48035a759dd048
Armand Nahimana <57989037+Trezzmann@users.noreply.github.com>
2022-02-27 14:26:32 +0200

    setup apollo client

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql
+} from "@apollo/client";
 
 import App from "./App";
+
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache()
+});
+
+client
+  .query({
+    query: gql`
+      query {
+        characters {
+          results {
+            name
+          }
+        }
+      }
+    `
+  })
+  .then((results) => console.log(results));
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
   rootElement
 );
